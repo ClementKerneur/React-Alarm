dc10ad0cef9acd86b31e97d24a58fd81822eda51
Clement Kerneur <clement@webmaitrise.net>
2016-03-26 12:21:03 +0400

    App Component

---
+++ js/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function () {
+    return (
+      <div className="hello">
+        Hello Alarm !!
+      </div>
+    );
+  }
+
+});
+
+module.exports = App;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-alert("Starting Project")
+var App = require( './components/App.js' );
+var ReactDom = require( 'react-dom' );
+var React = require( 'react' );
+
+ReactDom.render( <App />, document.getElementById('app') );

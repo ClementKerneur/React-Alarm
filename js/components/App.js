5fae1eda6120339134316163c0387c67e4b5201c
Clement Kerneur <clement@webmaitrise.net>
2016-03-26 12:47:29 +0400

    Alarm List

--- js/components/App.js
+++ js/components/App.js
@@ -1,11 +1,12 @@
-var React = require('react');
+var React = require( 'react' );
+var AlarmList = require( './AlarmList.js' );
 
 var App = React.createClass({
 
   render: function () {
     return (
-      <div className="hello">
-        Hello Alarm !!
+      <div className="App">
+        <AlarmList />
       </div>
     );
   }

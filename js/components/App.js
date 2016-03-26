343e2f71546c75aa2e92e333d90e5370a6474d7d
Jeremy Barthoux <barthoux.jeremy@gmail.com>
2016-03-26 17:02:21 +0400

    add new alarm

--- js/components/App.js
+++ js/components/App.js
@@ -1,19 +1,12 @@
 var React = require( 'react' );
 
-var AlarmListItem = React.createClass({
+var App = React.createClass({
 
-  render: function() {
-    return ( 
-      <li className="alarm">
-        <div className="description">
-          <h2 className="name">
-            {this.props.data.name}
-          </h2>
-          <p className="time">
-            {this.props.data.time}
-          </p>
-        </div>
-      </li>
+  render: function () {
+    return (
+      <div className="App">
+        <AlarmList />
+      </div>
     );
   }
 
@@ -36,6 +29,22 @@ var AlarmList = React.createClass({
     };
   },
 
+  _onAddAlarm: function(event) {
+    event.preventDefault();
+
+    var newAlarm = {
+      name : this.refs.name.value,
+      time : this.refs.time.value
+    }
+
+    var tmpAlarms = this.state.alarms;
+    tmpAlarms.push(newAlarm);
+
+    this.setState({
+      alarms: tmpAlarms
+    });
+  },
+
   render: function() {
     var alarms = this.state.alarms.map( function(alarm) {
       return <AlarmListItem data={alarm} />
@@ -44,6 +53,11 @@ var AlarmList = React.createClass({
     return (
       <div>
         <h1>React Alarm</h1>
+        <form onSubmit={this._onAddAlarm} >
+          <input type="text" placeholder="Name" ref="name"/>
+          <input type="text" placeholder="Time" ref="time"/>
+          <button type="submit">Add value</button>
+        </form>
         <ul className="alarms">
           {alarms}
         </ul>
@@ -55,13 +69,20 @@ var AlarmList = React.createClass({
 });
 
 
-var App = React.createClass({
+var AlarmListItem = React.createClass({
 
-  render: function () {
-    return (
-      <div className="App">
-        <AlarmList />
-      </div>
+  render: function() {
+    return ( 
+      <li className="alarm">
+        <div className="description">
+          <h2 className="name">
+            {this.props.data.name}
+          </h2>
+          <p className="time">
+            {this.props.data.time}
+          </p>
+        </div>
+      </li>
     );
   }
 
